c82c666382d3ee4928c034824145415daa8f5551
StutiBansal21 <stutibansal99@gmail.com>
2023-03-09 08:20:50 +0800

    first commit

---
+++ Week2/test1.py
@@ -0,0 +1,43 @@
+# lecture 2 week 2 for the semester class for Programming for Business. BUSN5101
+" introduction to python and features of python. Downloading the python and IDE working the first print statement"
+print("Hello World!")
+print(3 * 4)
+
+# VARIABLES
+my_name = "Stuti"
+print(my_name)
+x = 3.5
+print(x)
+
+# NameError occurs when you try to use a variable that is not defined
+# print(t)
+
+firstName = "Harry"
+first_Name = "ron"
+x1 = 10
+x2 = 12
+print(x1 + x2)
+
+a = b = c = 120
+print(a)
+print(b)
+print(c)
+
+m, n, o = 1, 2, 3
+print(m, n, o)
+# syntax error when issue with syntax of python
+# first name="Stuti"->invalid syntax
+
+# a,b,c=1,2 ValueError as the number of variables should have thir value defined
+
+# when the statement is too long then we break the statement using the escape back slash (you=3+my-2+5-4*my)
+my = 3
+print("I broke this line into 2 parts using the '\' symbol")
+you = 3 + my - 2 + \
+      5 - 4 * my
+print(you)
+
+# invalid syntax. x=2,y=4,print(x+y) instead use semicolon -> TO IMPROVE CLARITY
+x = 2; y = 4; print(x + y) # to print in 1 line!
+
+#what is none/null?
